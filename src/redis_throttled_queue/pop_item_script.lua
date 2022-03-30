4a56ff2f98986105155b43f26d46e095fd2f6e86
Ionel Cristian Mărieș <contact@ionelmc.ro>
2022-03-30 20:10:15 +0300

    Add total and do some renaming.

--- src/redis_throttled_queue/pop_item_script.lua
+++ src/redis_throttled_queue/pop_item_script.lua
@@ -9,45 +9,47 @@ ARGV arguments:
 
 Typical key structure (for every ITEM returned M is incremented, but only while M < limit):
 
-    PREFIX:usage:WINDOW - zlist of:
-        (M, NAME)
-    PREFIX:queue:NAME - list of:
-        ITEM
+    PREFIX:usage:WINDOW - zlist of (M, NAME)
+    PREFIX:queue:NAME - list of ITEM
 ]]
 if #ARGV ~= 4 then
     error("POP_ITEM_SCRIPT expected 4 arguments, but got " .. #ARGV .. " arguments!")
 end
 local PREFIX, WINDOW, LIMIT, RESOLUTION = unpack(ARGV)
 LIMIT = tonumber(LIMIT)
-local usage_zset_key = PREFIX .. ":usage:" .. WINDOW
-local name = redis.call("ZRANGE", usage_zset_key, 0, "(" .. LIMIT, "BYSCORE", "LIMIT", 0, 1)[1]
-redis.call('SET', 'DEBUG', 'ZRANGE ' .. usage_zset_key .. ' => ' .. tostring(name))
+local usage_key = PREFIX .. ":usage:" .. WINDOW
+local total_key = PREFIX .. ":queue-total"
+
+local name = redis.call("ZRANGE", usage_key, 0, "(" .. LIMIT, "BYSCORE", "LIMIT", 0, 1)[1]
+-- redis.call('SET', 'DEBUG', 'ZRANGE ' .. usage_zset_key .. ' => ' .. tostring(name))
 if name ~= nil then
     local queue_list_key = PREFIX .. ":queue:" .. name
     local value = redis.call("ZPOPMAX", queue_list_key)
-    redis.call('SET', 'DEBUG', 'ZPOPMAX ' .. queue_list_key .. ' => ' .. tostring(value[1]))
+    -- redis.call('SET', 'DEBUG', 'ZPOPMAX ' .. queue_list_key .. ' => ' .. tostring(value[1]))
     if value ~= nil then
-        redis.call("ZINCRBY", usage_zset_key, 1, name)
-        redis.call("EXPIRE", usage_zset_key, RESOLUTION)
+        redis.call("ZINCRBY", usage_key, 1, name)
+        redis.call("EXPIRE", usage_key, RESOLUTION)
+        redis.call("DECR", total_key)
         return value[1]
     end
 end
 local cursor = "0"
-local queue_list_key_pattern = PREFIX .. ":queue:*"
-local queue_list_key_prefix_len = #queue_list_key_pattern
-redis.call('SET', 'DEBUG', 'SCAN ' .. queue_list_key_pattern)
+local queue_key_pattern = PREFIX .. ":queue:*"
+local queue_key_prefix_len = #queue_key_pattern
+-- redis.call('SET', 'DEBUG', 'SCAN ' .. queue_list_key_pattern)
 repeat
-    local result = redis.pcall("SCAN", cursor, "MATCH", queue_list_key_pattern, "COUNT", 1000)
-    local queue_list_keys = result[2]
-    if queue_list_keys ~= nil then
-        for _, queue_list_key in ipairs(queue_list_keys) do
-            local name = string.sub(queue_list_key, queue_list_key_prefix_len, -1)
-            local score = redis.call("ZSCORE", usage_zset_key, name)
+    local result = redis.pcall("SCAN", cursor, "MATCH", queue_key_pattern, "COUNT", 1000)
+    local queue_keys = result[2]
+    if queue_keys ~= nil then
+        for _, queue_key in ipairs(queue_keys) do
+            local name = string.sub(queue_key, queue_key_prefix_len, -1)
+            local score = redis.call("ZSCORE", usage_key, name)
             if tonumber(score or 0) < LIMIT then
-                local value = redis.call("ZPOPMAX", queue_list_key)
+                local value = redis.call("ZPOPMAX", queue_key)
                 if value ~= nil then
-                    redis.call("ZINCRBY", usage_zset_key, 1, name)
-                    redis.call("EXPIRE", usage_zset_key, RESOLUTION)
+                    redis.call("ZINCRBY", usage_key, 1, name)
+                    redis.call("EXPIRE", usage_key, RESOLUTION)
+                    redis.call("DECR", total_key)
                     return value[1]
                 end
             end
