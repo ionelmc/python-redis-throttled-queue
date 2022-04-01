9a2aea6d23d40826c32a1f04dff3304519eba703
Ionel Cristian Mărieș <contact@ionelmc.ro>
2022-04-01 23:40:42 +0300

    Refactor duplicated code.

--- src/redis_throttled_queue/pop_item_script.lua
+++ src/redis_throttled_queue/pop_item_script.lua
@@ -22,53 +22,43 @@ LIMIT = tonumber(LIMIT)
 local usage_key = PREFIX .. ":usage:" .. WINDOW
 local total_key = PREFIX .. ":total"
 local names_key = PREFIX .. ":names"
-
-local names = redis.call("ZRANGE", usage_key, 0, "(" .. LIMIT, "BYSCORE")
-for _, name in ipairs(names) do
-    --redis.call('SET', 'DEBUG', 'ZRANGE ' .. usage_key .. ' => ' .. tostring(name))
-    local queue_key = PREFIX .. ":queue:" .. name
-    local highest_item = redis.call("ZPOPMAX", queue_key)
-    --redis.call('SET', 'DEBUG', 'ZPOPMAX ' .. queue_key .. ' => #' .. tostring(#highest_item))
-    if #highest_item ~= 0 then
-        local value = highest_item[1]
-        redis.call("ZINCRBY", usage_key, 1, name)
-        redis.call("EXPIRE", usage_key, RESOLUTION)
-        redis.call("DECR", total_key)
-        --redis.call('SET', 'DEBUG', '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   returning: ' .. tostring(highest_item[1]))
-        return value
-    end
-end
-
-local cursor = "0"
-repeat
-    local result = redis.call("SSCAN", names_key, cursor, "COUNT", 1000)
-    --redis.call('SET', 'DEBUG', 'SSCAN ' .. names_key .. ' ' .. cursor .. " COUNT 1000 => @" .. result[1] .. ', ' .. type(result[2]))
-    local names = result[2]
-    if names ~= nil then
-        for _, name in ipairs(names) do
-            local queue_key = PREFIX .. ":queue:" .. name
-            if redis.call('EXISTS', queue_key) then
-                redis.call("ZINCRBY", usage_key, 0, name)
-            else
-                redis.call("SREM", names_key, name)
-            end
+local function query_usage()
+    local names = redis.call("ZRANGE", usage_key, 0, "(" .. LIMIT, "BYSCORE")
+    for _, name in ipairs(names) do
+        --redis.call('SET', 'DEBUG', 'ZRANGE ' .. usage_key .. ' => ' .. tostring(name))
+        local queue_key = PREFIX .. ":queue:" .. name
+        local highest_item = redis.call("ZPOPMAX", queue_key)
+        --redis.call('SET', 'DEBUG', 'ZPOPMAX ' .. queue_key .. ' => #' .. tostring(#highest_item))
+        if #highest_item ~= 0 then
+            local value = highest_item[1]
+            redis.call("ZINCRBY", usage_key, 1, name)
+            redis.call("EXPIRE", usage_key, RESOLUTION)
+            redis.call("DECR", total_key)
+            --redis.call('SET', 'DEBUG', '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   returning: ' .. tostring(highest_item[1]))
+            return value
         end
     end
-    cursor = result[1]
-until cursor == "0"
-
-local names = redis.call("ZRANGE", usage_key, 0, "(" .. LIMIT, "BYSCORE")
-for _, name in ipairs(names) do
-    --redis.call('SET', 'DEBUG', 'ZRANGE ' .. usage_key .. ' => ' .. tostring(name))
-    local queue_key = PREFIX .. ":queue:" .. name
-    local highest_item = redis.call("ZPOPMAX", queue_key)
-    --redis.call('SET', 'DEBUG', 'ZPOPMAX ' .. queue_key .. ' => #' .. tostring(#highest_item))
-    if #highest_item ~= 0 then
-        local value = highest_item[1]
-        redis.call("ZINCRBY", usage_key, 1, name)
-        redis.call("EXPIRE", usage_key, RESOLUTION)
-        redis.call("DECR", total_key)
-        --redis.call('SET', 'DEBUG', '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   returning: ' .. tostring(highest_item[1]))
-        return value
-    end
 end
+
+local value = query_usage()
+if value == nil then
+    local cursor = "0"
+    repeat
+        local result = redis.call("SSCAN", names_key, cursor, "COUNT", 1000)
+        --redis.call('SET', 'DEBUG', 'SSCAN ' .. names_key .. ' ' .. cursor .. " COUNT 1000 => @" .. result[1] .. ', ' .. type(result[2]))
+        local names = result[2]
+        if names ~= nil then
+            for _, name in ipairs(names) do
+                local queue_key = PREFIX .. ":queue:" .. name
+                if redis.call('EXISTS', queue_key) then
+                    redis.call("ZINCRBY", usage_key, 0, name)
+                else
+                    redis.call("SREM", names_key, name)
+                end
+            end
+        end
+        cursor = result[1]
+    until cursor == "0"
+    value = query_usage()
+end
+return value
